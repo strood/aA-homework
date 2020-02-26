c8aa38cd979dfcb627f8a4e7bacedd7fb73e1912
strood <stroodz19@gmail.com>
2020-02-26 14:53:34 -0700

    Error handling funtime completed

---
+++ W1D3/error-handle-skeleton/user_script.rb
@@ -0,0 +1,11 @@
+require_relative "super_useful"
+
+puts "'five' == #{convert_to_int("five")}"
+
+feed_me_a_fruit
+
+sam = BestFriend.new("Barry", 5, "Checkers")
+
+sam.talk_about_friendship
+sam.do_friendstuff
+sam.give_friendship_bracelet
