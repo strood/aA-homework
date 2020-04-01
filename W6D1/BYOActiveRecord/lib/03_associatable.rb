4a0ac9aa3dfe0fa7b32c219452fc980d14f00891
strood <stroodz19@gmail.com>
2020-03-31 18:55:59 -0600

    BYOActiveRecord Part 2

--- W6D1/BYOActiveRecord/lib/03_associatable.rb
+++ W6D1/BYOActiveRecord/lib/03_associatable.rb
@@ -1,5 +1,5 @@
-require_relative '02_searchable'
-require 'active_support/inflector'
+require_relative "02_searchable"
+require "active_support/inflector"
 
 # Phase IIIa
 class AssocOptions
@@ -10,23 +10,43 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    # return class name of the model called on
+    @class_name.constantize
   end
 
   def table_name
-    # ...
+    # returns table name of the model called on
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    # provide default values for the 3 attributes for a new SQLObject
+    # :foreign_key, :primary_key, :class_name
+    defaults = {
+      :foreign_key => "#{name}_id".to_sym,
+      :primary_key => :id,
+      :class_name => name.to_s.camelcase,
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id,
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
@@ -34,17 +54,36 @@ module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
     # ...
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => key_val).first
+    end
   end
 
   def has_many(name, options = {})
     # ...
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
