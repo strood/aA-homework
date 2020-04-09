a6b9d6039ea777472646865f910d63ecde4834fa
strood <stroodz19@gmail.com>
2020-04-09 13:01:27 -0600

    Added forms, handled input, refactored with partials

--- W7D3/LibraryDemo/app/controllers/application_controller.rb
+++ W7D3/LibraryDemo/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+
+# REMOVE THIS FOR PRODUCTION< JUST BAVOIDS US HAVING AUTHENTICITY ISSUES WHNE TESTING
+skip_before_action :verify_authenticity_token
 end
--- W7D3/LibraryDemo/app/controllers/books_controller.rb
+++ W7D3/LibraryDemo/app/controllers/books_controller.rb
@@ -13,11 +13,47 @@ class BooksController < ApplicationController
     unless @book
       # return user to inmdex page if not found
       redirect_to books_url
-      return 
+      return
     end
 
     render :show
+  end
 
+  def create
+    # Shouldnt pass in raw params data to model, filtrer through the book_params
+    @book = Book.new(book_params) #This line creates a model level instance that we can then access from the pages we render, or gets us url redirects ect
 
+    if @book.save!
+      # show user the book show page
+      redirect_to book_url(@book)
+    else
+      # redirect back to the book form, save the info with tags on the new html page
+      render :new
+    end
+  end
+
+  def new
+      @book = Book.new
+      render :new
+  end
+
+  def edit
+    @book = Book.find_by(id: params[:id] )
+    render :edit
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+
+    if @book.update_attributes(book_params)
+      redirect_to book_url(@book)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :year, :category, :description)
   end
 end
