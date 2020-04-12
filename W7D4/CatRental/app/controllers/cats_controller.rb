019f2ca99a46700a0c4baba1dbb8618cff188f38
strood <stroodz19@gmail.com>
2020-04-12 16:45:38 -0600

    Completed edit page, with routing

--- W7D4/CatRental/app/controllers/cats_controller.rb
+++ W7D4/CatRental/app/controllers/cats_controller.rb
@@ -24,6 +24,39 @@ class CatsController < ApplicationController
   end
 
   def create
+    @cat = Cat.new(cat_params)
 
+    if @cat.save!
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat  = Cat.find(params[:id])
+    unless @cat
+      # return user to inmdex page if not found
+      redirect_to cats_url
+      return
+    end
+
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+
+    if @cat.update(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name,:birth_date,:sex,:color,:description)
   end
 end
